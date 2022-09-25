d7c6e700cb4de49d5985a95cb2367a31022ea6db
Andrew Uzhva <103964378+trant755@users.noreply.github.com>
2022-09-25 23:18:28 +0300

    MARK 2

--- src/index.js
+++ src/index.js
@@ -7,27 +7,49 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const refs = {
   searchForm: document.querySelector('#search-form'),
   gallery: document.querySelector('.gallery'),
+  observerEl: document.querySelector('#observer'),
 };
 
 const imageAPI = new ImageAPI();
+let lightbox = new SimpleLightbox('.gallery a');
 
 refs.searchForm.addEventListener('submit', searchImages);
 
 function searchImages(e) {
   e.preventDefault();
   imageAPI.query = e.currentTarget.elements.searchQuery.value;
-  console.log(imageAPI.query);
 
   if (imageAPI.query === '') return;
-  fetchImages();
+
+  clearGallery();
+  imageAPI.resetPage();
+
+  imageAPI.fetchImages().then(({ data }) => {
+    if (data.hits.length === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      ),
+        { timeout: 4000 };
+      return;
+    }
+
+    Notify.info(`Hooray! We found ${data.totalHits} images.`),
+      { timeout: 5000 };
+
+    createPage(data);
+  });
 }
 
-function fetchImages() {
-  return imageAPI.fetchImages().then(({ data }) => {
-    let markup = data.hits.map(creatPhotoCard).join('');
+function clearGallery() {
+  refs.gallery.innerHTML = '';
+}
 
-    refs.gallery.innerHTML = markup;
-  });
+function createPage(data) {
+  let markup = data.hits.map(creatPhotoCard).join('');
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
+
+  imageAPI.incrementPage();
+  lightbox.refresh();
 }
 
 function creatPhotoCard({
@@ -40,24 +62,60 @@ function creatPhotoCard({
   downloads,
 }) {
   return `
-      <a href=${largeImageURL}>
+      <a class="gallery__lightbox-link"  href=${largeImageURL}>
         <div class="photo-card">
-          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+          <img class="photo-card__img" src="${webformatURL}" alt="${tags}" loading="lazy" />
           <div class="info">
             <p class="info-item">
-              <b>Likes</b>${likes}
+              <b>Likes</b>
+              <span>${likes}</span>
             </p>
             <p class="info-item">
-              <b>Views</b>${views}
+              <b>Views</b>
+              <span>${views}</span>
             </p>
             <p class="info-item">
-              <b>Comments</b>${comments}
+              <b>Comments</b>
+              <span>${comments}</span>
             </p>
             <p class="info-item">
-              <b>Downloads</b>${downloads}
+              <b>Downloads</b>
+              <span>${downloads}</span>
             </p>
           </div>
         </div>
       </a>
 `;
 }
+
+const onEntry = entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting && imageAPI.page !== 1) {
+      imageAPI.fetchImages().then(({ data }) => {
+        if (imageAPI.sumImg > data.totalHits) {
+          Notify.info(
+            "We're sorry, but you've reached the end of search results."
+          ),
+            { timeout: 10000 };
+
+          return;
+        }
+
+        if (data.hits.length === 0) {
+          Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          ),
+            { timeout: 4000 };
+          return;
+        }
+
+        createPage(data);
+      });
+    }
+  });
+};
+
+const observer = new IntersectionObserver(onEntry, {
+  rootMargin: '200px',
+});
+observer.observe(refs.observerEl);
--- src/js/Image-api.js
+++ src/js/Image-api.js
@@ -6,16 +6,34 @@ const BASE_URL = 'https://pixabay.com/api/';
 export default class ImageAPI {
   constructor() {
     this.searchQuery = '';
-    this.searchParams = new URLSearchParams({
+    this.page = 1;
+    this.sumImg = 0;
+  }
+
+  async fetchImages() {
+    const searchParams = new URLSearchParams({
+      q: this.searchQuery,
       key: KEY,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
+      page: this.page,
+      per_page: 40,
     });
+    try {
+      this.incrementSumImg();
+      return await axios.get(`${BASE_URL}?${searchParams}`);
+    } catch (error) {
+      Notify.failure('ERROR'), { timeout: 3000 };
+    }
   }
 
-  fetchImages() {
-    return axios.get(`${BASE_URL}?q=${this.searchQuery}&${this.searchParams}`);
+  incrementPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
   }
 
   get query() {
@@ -24,5 +42,13 @@ export default class ImageAPI {
 
   set query(newQuery) {
     this.searchQuery = newQuery;
+  }
+
+  incrementSumImg() {
+    this.sumImg += 40;
+  }
+
+  resetSumImg() {
+    this.sumImg = 0;
   }
 }
